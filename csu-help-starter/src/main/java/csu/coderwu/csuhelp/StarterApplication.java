c00b4c6431c2dc217e78efed160beff9fa8ea753
coderWu <coderwumail@gmail.com>
2018-05-24 19:35:31 +0800

    创建项目

---
+++ csu-help-starter/src/main/java/csu/coderwu/csuhelp/StarterApplication.java
@@ -0,0 +1,20 @@
+package csu.coderwu.csuhelp;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {
+        "csu.coderwu.csuhelp",
+        "csu.coderwu.csuhelp.core",
+        "csu.coderwu.csuhelp.db",
+        "csu.coderwu.csuhelp.wx.mini"
+
+})
+@MapperScan("csu.coderwu.csuhelp.db.do")
+public class StarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterApplication.class, args);
+    }
+}
---
+++ csu-help-starter/src/test/java/csu/coderwu/csuhelp/CsuHelpStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package csu.coderwu.csuhelp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CsuHelpStarterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
